e46802622d5cb921c7dfb7e5b04f18863db2eaf9
prabakaran <praba104016@gmail.com>
2025-02-17 13:16:26 +0530

    nav response

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,52 +1,32 @@
-import { Link } from 'react-router-dom';
-import './NavBar.css'
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
+
 function NavBar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <>
+            <div className="nav">
+                <div className="container">
+                    <Link to="./" className="logo">MyPortfolio</Link>
 
-            <div class="nav">
-                <div class="container">
-                    <Link to="./">  <div class="btn"><h3>Home</h3></div></Link>
-                    <Link to="./About">    <div class="btn"><h3>About</h3></div></Link>
-                    <Link to="./Project">   <div class="btn"><h3>Project</h3></div></Link>
-                    <Link to="./Contact"> <div class="btn"><h3>Contact</h3></div></Link>
-                    <svg
-                        class="outline"
-                        overflow="visible"
-                        width="400"
-                        height="60"
-                        viewBox="0 0 400 60"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <rect
-                            class="rect"
-                            pathLength="100"
-                            x="0"
-                            y="3"
-                            width="1532"
-                            height="60"
-                            fill="transparent"
-                            stroke-width="5"
-                        ></rect>
-                    </svg>
+                    {/* Hamburger Menu Icon */}
+                    <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+                        ☰
+                    </div>
+
+                    {/* Navigation Links */}
+                    <div className={`nav-links ${menuOpen ? "open" : ""}`}>
+                        <Link to="./" onClick={() => setMenuOpen(false)}>Home</Link>
+                        <Link to="./About" onClick={() => setMenuOpen(false)}>About</Link>
+                        <Link to="./Project" onClick={() => setMenuOpen(false)}>Projects</Link>
+                        <Link to="./Contact" onClick={() => setMenuOpen(false)}>Contact</Link>
+                    </div>
                 </div>
             </div>
-
-
         </>
     );
 }
 
 export default NavBar;
-
-{/* <div className="navbar">
-    <Link to="./"> <button className='btn'>Home</button></Link>
-    <Link to="./About">   <button className='btn'>  About </button></Link>
-    <Link to="./Project">  <button className='btn'> Projects </button></Link>
-    <Link to="./Contact">  <button className='btn'> Contact</button></Link>
-</div> */}
-
-// <Link to="./">  <div class="btn">Home</div></Link>
-// <Link to="./About">    <div class="btn">Contact</div></Link>
-// <Link to="./Project">   <div class="btn">About</div></Link>
-// <Link to="./Contact"> <div class="btn">About</div></Link>
